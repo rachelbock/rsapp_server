894f12eee0a56718f665b526cf9ebaf7744f7b3e
Rachel Bock <rachel.bock@Rachel-Bocks-MacBook-Pro.local>
2017-10-30 13:16:23 -0700

    Adds token table and resource

---
+++ src/main/java/com/rachelbock/PostNotificationSender.java
@@ -0,0 +1,10 @@
+package com.rachelbock;
+
+/**
+ * Sends notification to registered apps.
+ */
+public class PostNotificationSender {
+
+    private static final String ARN = "arn:aws:sns:us-west-2:432593804115:endpoint/APNS_SANDBOX/FloorManager/04e7f818-bee6-3353-a2f8-9fa8e7a94217";
+
+}
--- src/main/java/com/rachelbock/resources/MessageResource.java
+++ src/main/java/com/rachelbock/resources/MessageResource.java
@@ -3,9 +3,11 @@ package com.rachelbock.resources;
 import com.rachelbock.data.Message;
 import com.rachelbock.db.ConnectionPool;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -33,9 +35,9 @@ public class MessageResource {
                 message.setMessageId(resultSet.getInt("id"));
                 message.setRecepName(resultSet.getString("recep_name"));
                 message.setUrgent(resultSet.getBoolean("urgent"));
-                message.setMessageDate(resultSet.getDate("message_date"));
+                message.setMessageDate(resultSet.getTimestamp("message_date"));
                 message.setLocation(resultSet.getString("location"));
-                message.setReceivedDate(resultSet.getDate("received_date"));
+                message.setReceivedDate(resultSet.getTimestamp("received_date"));
                 message.setReceivedBy(resultSet.getString("received_by"));
                 message.setTeamName(resultSet.getString("team_name"));
 
@@ -65,9 +67,9 @@ public class MessageResource {
                 message.setMessageId(resultSet.getInt("id"));
                 message.setRecepName(resultSet.getString("recep_name"));
                 message.setUrgent(resultSet.getBoolean("urgent"));
-                message.setMessageDate(resultSet.getDate("message_date"));
+                message.setMessageDate(resultSet.getTimestamp("message_date"));
                 message.setLocation(resultSet.getString("location"));
-                message.setReceivedDate(resultSet.getDate("received_date"));
+                message.setReceivedDate(resultSet.getTimestamp("received_date"));
                 message.setReceivedBy(resultSet.getString("received_by"));
                 message.setTeamName(resultSet.getString("team_name"));
 
@@ -75,28 +77,28 @@ public class MessageResource {
             }
             return messages;
         } catch (SQLIntegrityConstraintViolationException e) {
-            throw new NotFoundException("Could not find any open messages");
+            throw new NotFoundException("Could not find any messages");
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
 
-        throw new NotFoundException("Could not find any open messages");
+        throw new NotFoundException("Could not find any messages");
     }
 
     @POST
     @Path("new_message")
-    public Message addNewMessage(NewMessageRequest newMessageRequest) {
+    public Response addNewMessage(NewMessageRequest newMessageRequest) {
         try (Connection conn = ConnectionPool.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement("INSERT INTO message (recep_name, team_name, " +
-                    "location, message, message_date) VALUES ?,?,?,?");
+                    "location, urgent, message_date) VALUES (?,?,?,?,?)");
 
             stmt.setString(1, newMessageRequest.getRecepName());
             stmt.setString(2, newMessageRequest.getTeamName());
             stmt.setString(3, newMessageRequest.getLocation());
             stmt.setBoolean(4, newMessageRequest.isUrgent());
-            stmt.setDate(5, new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+            stmt.setTimestamp(5, new java.sql.Timestamp(Calendar.getInstance().getTimeInMillis()));
             stmt.execute();
-
+        return Response.ok("Successfully created new message").build();
         } catch (SQLIntegrityConstraintViolationException e) {
             throw new BadRequestException("Duplicate Message");
         } catch (SQLException e1) {
@@ -104,99 +106,28 @@ public class MessageResource {
         }
         throw new InternalServerErrorException("Internal Error - could not create new message");
     }
-//
-//    @PUT
-//    @Path("update_message")
-//    public Message updateMessage(@PathParam("messageId") String messageId) {
-//
-//    }
 
-//    /**
-//     * Retrieves User information for a given username. If no User exists with the username a NotFound
-//     * Exception is thrown.
-//     * @param userName - used in query to match user by username
-//     * @return - if there is a matching User, the User is returned.
-//     */
-//    @GET
-//    @Path("/{messageId}")
-//    public Message getUserByUserName(@PathParam("messageId") String userName) {
-//
-//
-//        try (Connection conn = ConnectionPool.getConnection();
-//             Statement stmt = conn.createStatement();
-//             ResultSet resultSet = stmt.executeQuery("SELECT * FROM user_information WHERE user_name ='" + userName + "'")) {
-//
-//            if (resultSet.next()) {
-//                User user = new User();
-//                user.setUserName(userName);
-//                user.setName(resultSet.getString("name"));
-//                user.setHeight(resultSet.getInt("height"));
-//                user.setSkillLevel(resultSet.getInt("skill_level"));
-//
-//                return user;
-//            }
-//        } catch (SQLIntegrityConstraintViolationException e) {
-//            throw new BadRequestException("Could not find user with username " + userName );
-//        } catch (SQLException e1) {
-//            e1.printStackTrace();
-//        }
-//
-//        throw new NotFoundException("Could not find user with username " + userName);
-//    }
-//
-//
-//    /**
-//     * Method to add a new User to the Clamber Database. If it is unable to add the User, it throws an
-//     * exception.
-//     * @param request - json for the User
-//     * @return - if successful it will return the User.
-//     */
-//    @POST
-//    public User addUserToDatabase(NewUserRequest request) {
-//        try (Connection conn = ConnectionPool.getConnection();
-//             Statement stmt = conn.createStatement()) {
-//
-//            stmt.execute("INSERT INTO user_information (user_name, height, skill_level) VALUES " +
-//                    "('" + request.getUsername() + "', " + request.getHeight() + ", " + request.getSkill() + ")");
-//            return getUserByUserName(request.getUsername());
-//
-//        } catch (SQLIntegrityConstraintViolationException e) {
-//            throw new BadRequestException("Username already exists");
-//        } catch (SQLException e1) {
-//            e1.printStackTrace();
-//        }
-//        throw new InternalServerErrorException("Could not create new user " + request.getUsername());
-//    }
-//
-//    public static final String UPDATE_USER_QUERY = "UPDATE user_information SET height = ?, skill_level = ? \n" +
-//            "WHERE user_name = ?";
-//
-//    /**
-//     * Method to update an existing user in the database.
-//     * @param request - json for the User
-//     * @return - updated User
-//     */
-//    @Path("/{username}/update")
-//    @POST
-//    public User updateExistingUser(@PathParam("username") String username, NewUserRequest request) {
-//        try(Connection conn = ConnectionPool.getConnection()){
-//            PreparedStatement stmt = conn.prepareStatement(UPDATE_USER_QUERY);
-//            stmt.setInt(1, request.getHeight());
-//            stmt.setInt(2, request.getSkill());
-//            stmt.setString(3, username);
-//
-//            stmt.execute();
-//
-//            return getUserByUserName(request.getUsername());
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        throw new InternalServerErrorException("Could not create new user");
-//    }
+    @PUT
+    @Path("update_message")
+    public Response updateMessage(UpdateMessageRequest updateMessageRequest) {
+        try (Connection conn = ConnectionPool.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE message SET received_by = ?, received_date = ? " +
+                    "WHERE id = ?");
 
-    //Table for device tokens - when the app is opened, hit api to give us a deviceId.
-    //
+            stmt.setString(1, updateMessageRequest.getReceivedBy());
+            stmt.setTimestamp(2, new java.sql.Timestamp(Calendar.getInstance().getTimeInMillis()));
+            stmt.setInt(3, updateMessageRequest.getMessageId());
+            stmt.execute();
+            return Response.ok("Successfully updated message").build();
+        } catch (SQLIntegrityConstraintViolationException e) {
+            throw new NotFoundException("Could not find the message to update");
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        throw new InternalServerErrorException("Internal Error - could not update message");
+    }
+
+
 
     /**
      * Defines how we expect the json in the body to look
@@ -240,9 +171,9 @@ public class MessageResource {
         }
     }
 
-    public static class updateMessageRequest {
+    public static class UpdateMessageRequest {
         protected int messageId;
-        protected String claimedBy;
+        protected String receivedBy;
 
         public int getMessageId() {
             return messageId;
@@ -252,12 +183,12 @@ public class MessageResource {
             this.messageId = messageId;
         }
 
-        public String getClaimedBy() {
-            return claimedBy;
+        public String getReceivedBy() {
+            return receivedBy;
         }
 
-        public void setClaimedBy(String claimedBy) {
-            this.claimedBy = claimedBy;
+        public void setReceivedBy(String receivedBy) {
+            this.receivedBy = receivedBy;
         }
     }
 }
